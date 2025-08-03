ca37fe1a46a7fb862a6d07edbc0297ac22b6f17a
Ali Ibtehaj Asif Niazi <aliibtehajasifniazi@gmail.com>
2025-08-03 16:05:28 +0100

    task service created

---
+++ task-service/src/main/java/com/example/taskservice/controller/TaskController.java
@@ -0,0 +1,32 @@
+package com.example.taskservice.controller;
+
+import com.example.taskservice.model.Task;
+import com.example.taskservice.service.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable String id) {
+        return taskService.getTaskById(id);
+    }
+}
